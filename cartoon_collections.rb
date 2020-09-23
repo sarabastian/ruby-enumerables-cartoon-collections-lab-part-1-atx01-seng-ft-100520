0a209c6f96bde312055d043a233b5996700c85f0
Sara Bastian <seb118@georgetown.edu>
2020-09-23 16:25:09 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,7 @@ def list_dwarves(array)
   array.each_with_index do |name, index|
     index = 0
     array[name] = index
-    puts "#{index.to_i + 1}. #{name}"
+    puts "#{index + 1}. #{name}"
     
   end 
   # Use `each_with_index` to enumerate over the provided array
